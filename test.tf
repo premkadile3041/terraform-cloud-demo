470138d9eebcf8a9a46019a15d7c6a01b7344d19
root <root@ip-172-16-1-251.us-west-2.compute.internal>
2020-05-28 07:59:11 +0000

    added region

--- test.tf
+++ test.tf
@@ -1,4 +1,13 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
 resource "aws_instance" "basic" {
   ami           = "ami-0735e22e032b35e38"
   instance_type = "t3.nano"
+  tags = {
+    Name = "tf-cloud"
+   
+  }
+
 }
