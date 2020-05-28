43d2ad75e0bb5e044cbfffb189dfd30de3bcb1de
root <root@ip-172-16-1-251.us-west-2.compute.internal>
2020-05-28 09:05:55 +0000

    added ec2 instance

--- test.tf
+++ test.tf
@@ -10,3 +10,25 @@ provider "aws" {
   }
 
 }
+
+
+resource "aws_subnet" "pub" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0d6621c01e8c2de2c"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
+
