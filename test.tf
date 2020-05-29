914f41388867e7213e6955c76b1774316e467ac5
root <root@ip-172-16-1-251.us-west-2.compute.internal>
2020-05-29 02:53:41 +0000

    removed tags for vpc

--- test.tf
+++ test.tf
@@ -3,22 +3,12 @@ provider "aws" {
 }
   resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
-
-  tags = {
-    Name = "tf-cloud"
-   
-  }
-
 }
-
 
 resource "aws_subnet" "pub" {
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.1.0/24"
 
-  tags = {
-    Name = "Main"
-  }
 }
 
 resource "aws_instance" "web" {
