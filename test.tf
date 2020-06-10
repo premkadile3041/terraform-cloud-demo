229502180f90862e7f5ea8511eefd0e1782431b8
premkadile3041 <46262759+premkadile3041@users.noreply.github.com>
2020-06-10 11:51:13 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,23 +1,9 @@
 provider "aws" {
   region = "us-west-1"
 }
-  resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
-
-}
-
-resource "aws_subnet" "pub" {
-  vpc_id     = aws_vpc.main.id
-  cidr_block = "10.0.1.0/24"
-
-}
-
-resource "aws_instance" "web" {
-  ami           = "ami-0b91a410940e82c54"
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.pub.id
-
-
+  
+resource "aws_ecs_cluster" "hpd_fargate" {
+  name = "hpd_fargate"
 }
 
 
